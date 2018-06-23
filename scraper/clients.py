0e09154af982e7d9a026dd2857763a5dec93f1df
Simon Wörpel <simon.woerpel@medienrevolte.de>
2018-06-23 15:19:10 +0200

    refactor: make own project for pipeline

--- scraper/clients.py
+++
@@ -1,7 +0,0 @@
-from zeep import Client
-
-import settings
-
-
-ResearchClient = Client(wsdl=settings.GENESIS_SERVICES['research'], strict=False)
-ExportClient = Client(wsdl=settings.GENESIS_SERVICES['export'], strict=False)
