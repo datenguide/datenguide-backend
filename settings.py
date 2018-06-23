41a755bb649b3cd50c501188f22b3805478f9422
Simon Wörpel <simon.woerpel@medienrevolte.de>
2018-06-23 15:32:52 +0200

    Handle settings via os env

--- settings.py
+++ settings.py
@@ -6,31 +6,21 @@ settings for datenguide-backend
 import os
 
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+STORAGE = os.getenv('STORAGE', 'JSONFileStorage')
+
 # locations
-DATA_ROOT = os.path.join(BASE_DIR, 'data')
-DATA_SRC = os.path.join(DATA_ROOT, 'src')
-DATABASE = os.path.join(DATA_ROOT, 'db.p')
-DATABASE_CSV = os.path.join(DATA_ROOT, 'db.csv')
-DATA_TREE = os.path.join(DATA_ROOT, 'db.json')
-KEYS_TREE = os.path.join(DATA_ROOT, 'keys.json')
-KEYS_INFO = os.path.join(DATA_ROOT, 'keys_info.json')
-KEYS_DIR = os.path.join(DATA_ROOT, 'keys')
-KEYS_DTYPES = os.path.join(DATA_ROOT, 'keys_dtypes.json')
-IDS_FILE = os.path.join(DATA_ROOT, 'ids.csv')
-STORAGE = 'JSONFileStorage'
+DATA_ROOT = os.getenv('DATA_ROOT', os.path.join(BASE_DIR, 'data'))
+DATA_TREE = os.getenv('DATA_TREE', os.path.join(DATA_ROOT, 'tree.json'))
+KEYS_TREE = os.getenv('KEYS_TREE', os.path.join(DATA_ROOT, 'keys.json'))
+KEYS_INFO = os.getenv('KEYS_INFO', os.path.join(DATA_ROOT, 'keys_info.json'))
+DTYPES = os.getenv('DTYPES', os.path.join(DATA_ROOT, 'dtypes.json'))
+IDS_FILE = os.getenv('IDS_FILE', os.path.join(DATA_ROOT, 'ids.csv'))
 
-
-# genesis webservice
-GENESIS_SERVICES = {
-    'research': 'https://www.regionalstatistik.de/genesisws/services/RechercheService_2010?wsdl',
-    'export': 'https://www.regionalstatistik.de/genesisws/services/ExportService_2010?wsdl',
+# elastic
+ELASTIC = {
+    'HOST': os.getenv('ELASTIC_HOST', 'localhost'),
+    'PORT': os.getenv('ELASTIC_PORT', '9200'),
+    'INDEX': os.getenv('ELASTIC_INDEX', 'genesapi')
 }
-
-
-try:
-    from local_settings import *   # noqa
-except ImportError:
-    pass
